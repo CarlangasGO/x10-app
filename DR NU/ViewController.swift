c012d4b5512129a85d6ea9e99397bc2f800b2e73
Mikkel Malmberg <mikkel@brnbw.com>
2016-03-03 12:55:41 +0100

    Initial wrapper app

--- DR NU/ViewController.swift
+++ DR NU/ViewController.swift
@@ -1,25 +1,82 @@
-//
-//  ViewController.swift
-//  DR NU
-//
-//  Created by Mikkel Malmberg on 23/02/16.
-//  Copyright © 2016 DR Nyheder X. All rights reserved.
-//
-
 import UIKit
+import MediaPlayer
+import AVFoundation
 
-class ViewController: UIViewController {
+let base = ""
 
+class ViewController: UIViewController, UIWebViewDelegate {
+
+    @IBOutlet var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        reloadWebSite()
+        
+        let cc = MPRemoteCommandCenter.sharedCommandCenter()
+        cc.playCommand.addTarget(self, action: Selector("play"))
+        cc.pauseCommand.addTarget(self, action: Selector("pause"))
+        cc.nextTrackCommand.addTarget(self, action: Selector("nextTrack"))
+        cc.previousTrackCommand.addTarget(self, action: Selector("previousTrack"))
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setCategory(AVAudioSessionCategoryPlayback)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        self.becomeFirstResponder()
+    }
+    
+    override func canBecomeFirstResponder() -> Bool {
+        return true
+    }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        if motion == UIEventSubtype.MotionShake {
+            self.webView.reload()
+        }
+    }
+    
+    // Public
+    
+    func reloadWebSite() {
+        let req = NSURLRequest(URL: (NSURL(string: base)!))
+        self.webView.loadRequest(req)
+    }
+    
+    // Media callbacks
+    
+    func play() {
+        print(evalFn("play"))
+    }
+    
+    func pause() {
+        print(evalFn("pause"))
+    }
+    
+    func nextTrack() {
+        print(evalFn("next"))
+    }
+ 
+    func previousTrack() {
+        print(evalFn("prev"))
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func evalFn(fn: String) -> String? {
+        return self.webView.stringByEvaluatingJavaScriptFromString("Controls.\(fn)()")
     }
-
-
+    
+    // UIWebViewDelegate
+    
+    func webViewDidStartLoad(webView: UIWebView) {
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+    }
+    
+    func webView(webView: UIWebView, didFailLoadWithError error: NSError?) {
+        print(error)
+    }
 }
 
